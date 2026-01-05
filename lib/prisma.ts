9fe08a72749616b112c7dfbd616573eeff120d1f
Ip-Tec <otakhorpeter@gmail.com>
2026-01-05 17:30:40 +0100

    feat: initialize and export a singleton Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
-// import { PrismaClient } from "@/src/generated/client";
 import { PrismaClient } from "@/src/generated";
-// import { * as PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
